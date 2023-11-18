c4c6b802054fa27f7196a2ab45e302cb73b612cc
Anthony-GG <iacano5@outlook.com>
2023-11-18 15:03:38 -0500

    beginning basic work on get and post routes for program

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -1,1 +1,8 @@
-{\rtf1}
+const router = requre('express').Router();
+
+
+//logic for /api/thoughts, get route and post route delivery
+router.route('/').get(getThoughts).post(createThought);
+
+
+module.exports = router;
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,1 +1,8 @@
-{\rtf1}
+const router = requre('express').Router();
+
+
+//logic for /api/users, get route and post route delivery
+router.route('/').get(getUser).post(createUser);
+
+
+module.exports = router;
